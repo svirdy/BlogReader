eeb02303e7c95cc7739b580ec94600bc227ba07b
Sachin Virdy <svirdy1@gmail.com>
2015-10-09 23:19:01 -0700

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Sachin Virdy on 10/9/15.
+//  Copyright © 2015 Sachin Virdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReader
+//
+//  Created by Sachin Virdy on 10/9/15.
+//  Copyright © 2015 Sachin Virdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
